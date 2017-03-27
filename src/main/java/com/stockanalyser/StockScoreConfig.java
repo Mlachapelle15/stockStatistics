bd2a9925b32484dbaae3e92d36e7159ec663c357
Maxime Lachapelle <mlachapelle@coveo.com>
2017-03-27 07:06:51 -0400

    score target are now configurable in the yaml

---
+++ src/main/java/com/stockanalyser/StockScoreConfig.java
@@ -0,0 +1,21 @@
+package com.stockanalyser;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConfigurationProperties(prefix = "tickers")
+public class TickerListConfig {
+  private List<String> list;
+
+  TickerListConfig() {
+    this.list = new ArrayList<>();
+  }
+
+  public List<String> getList() {
+    return this.list;
+  }
+}
---
+++ src/main/java/com/stockanalyser/stock/BigDecimalDeserializer.java
@@ -0,0 +1,7 @@
+package com.stockanalyser.stock;
+
+/**
+ * Created by mlachapelle on 2017-03-27.
+ */
+public class BigDecimalDeserializer {
+}
